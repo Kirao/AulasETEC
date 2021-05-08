8b2b3e353a3d85be0ba49516b5b2e3a17bbf2b25
Kirao <guihenrique1231@hotmail.com>
2021-05-07 22:41:35 -0300

    Aula sobre Vetor

---
+++ Aula11_EntradaDeDados1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Aula11_EntradaDeDados1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Area de Variaveis
+
+            Console.Clear();
+            string frase,cor1,cor2,cor3;
+            string palavra1, palavra2, palavra3;
+            string[] palavras;
+
+            #endregion
+
+            #region Entrada Individual de Dados
+
+            //entrada de Dados
+            Console.WriteLine("===========================================================");
+            Console.Write("Digite uma frase.....: ");
+            frase = Console.ReadLine();
+            Console.Write("Digite a primeira Cor: ");
+            cor1 = Console.ReadLine();
+            Console.Write("Digite a segunda Cor.: ");
+            cor2 = Console.ReadLine();            
+            Console.Write("Digite a terceira Cor: ");
+            cor3 = Console.ReadLine();
+            Console.WriteLine("===========================================================");
+
+            //Saida de dados 1
+            Console.WriteLine("\n===========================================================");
+            Console.WriteLine($"A Frase: {frase} , tem as cores {cor1} , {cor2} e {cor3}");
+            Console.WriteLine("===========================================================");
+
+            #endregion
+
+            #region Entrada de Dados em Bloco (Cumulativa)
+            
+            //entrada de dados
+            Console.WriteLine("\n\n===========================================================");
+            Console.Write("Digite 3 palavras separadas por espaço: ");
+            palavras = Console.ReadLine().Split(' ');
+            Console.WriteLine("===========================================================");
+
+            //Processamento de dados
+            palavra1 = palavras[0];
+            palavra2 = palavras[1];
+            palavra3 = palavras[2];
+
+            //Saida de dados
+            Console.WriteLine("\n===========================================================");
+            Console.WriteLine($"{palavras[0]} , {palavras[1]} , {palavras[2]}");
+            Console.WriteLine($"{palavras[1]} , {palavras[2]} , {palavras[0]}");
+            Console.WriteLine($"{palavras[2]} , {palavras[0]} , {palavras[1]}");
+            Console.WriteLine($"{palavra1} , {palavra2} , {palavra3}");
+            Console.WriteLine("===========================================================");            
+
+            #endregion
+
+            #region Area de Encerramento do Programa
+            Console.WriteLine("\n\nPressione qualquer tecla para sair....");
+            Console.ReadKey();
+            Console.Clear();
+            #endregion
+        }
+    }
+}
---
+++ Aula11_EntradaDeDados2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula11_EntradaDeDados2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Area de Variavel
+
+            Console.Clear();
+            string[] dadosPessoais;
+            string nomePessoa;
+            char sexoPessoa;
+            int idadePessoa;
+            double alturaPessoa;
+
+            #endregion
+
+            #region Area do Programa
+
+            //Entrada de Dados
+            Console.Write("Digite: nome, sexo, idade, altura. Separados por espaço: ");
+            dadosPessoais = Console.ReadLine().Split(' ');
+
+            //Processamento de dados
+            nomePessoa = dadosPessoais[0];
+            sexoPessoa = char.Parse(dadosPessoais[1]);
+            idadePessoa = int.Parse(dadosPessoais[2]);
+            alturaPessoa = double.Parse(dadosPessoais[3]);
+
+            //Saida de dados
+            Console.WriteLine($"\nOlá {dadosPessoais[0]} voce é do sexo: {dadosPessoais[1]}, possui {dadosPessoais[2]} anos e sua altura é de: {dadosPessoais[3]}");
+            Console.WriteLine($"\nOlá {nomePessoa} voce é do sexo: {sexoPessoa}, possui {idadePessoa} anos e sua altura é de: {alturaPessoa.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            #endregion
+
+            #region Area de Encerramento do Programa
+
+            Console.WriteLine("\n\nPressione qualquer tecla para sair....");
+            Console.ReadKey();
+            Console.Clear();
+
+            #endregion
+        }
+    }
+}
