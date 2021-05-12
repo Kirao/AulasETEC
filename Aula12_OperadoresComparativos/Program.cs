dac3850492200be1a10ab8c269d357f6319e5554
Kirao <guihenrique1231@hotmail.com>
2021-05-11 23:04:50 -0300

    Novas aula sobre operadores

---
+++ Aula12_OperadoresComparativos/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Aula12_OperadoresComparativos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Area de declaração de VArs
+
+            Console.Clear();
+            int a = 10;
+            bool comparacao1, comparacao2, comparacao3, comparacao4, comparacao5, comparacao6, comparacao7, comparacao8;
+
+            #endregion
+
+            #region Bloco de Processamento
+
+            //Comparação maior menor
+            comparacao1 = a < 10;
+            comparacao2 = a < 20;
+            comparacao3 = a > 10;
+            comparacao4 = a > 05;
+
+            //diferença, maior igual, menor igual, igual
+            comparacao5 = a != 10;  //Diferença 
+            comparacao6 = a == 10;  //Igual Igual
+            comparacao7 = a >= 10;  //Maior ou Igual
+            comparacao8 = a <= 10;  //Menor ou igual
+
+            #endregion
+
+            #region Bloco de saida de Dados
+
+            Console.WriteLine("--------------------------------------------------------------------------------------");
+            Console.WriteLine("*** BLOCO DE COMPARAÇÃO (MAIOR >) OU (MENOR <) ***");
+            Console.WriteLine("--------------------------------------------------------------------------------------");
+            Console.WriteLine($"O valor atribuido a variavel a é: {a}");
+            Console.WriteLine($"Na verificação se [a < 10] o resultado é: {comparacao1}");
+            Console.WriteLine($"Na verificação se [a < 20] o resultado é: {comparacao2}");
+            Console.WriteLine($"Na verificação se [a > 10] o resultado é: {comparacao3}");
+            Console.WriteLine($"Na verificação se [a > 05] o resultado é: {comparacao4}");
+
+            Console.WriteLine("\n--------------------------------------------------------------------------------------");
+            Console.WriteLine("*** BLOCO DE COMPARAÇÃO (Diferente !=) (Menor igual <=) (Maior igual >=) (Igual ==)***");
+            Console.WriteLine("--------------------------------------------------------------------------------------");
+            Console.WriteLine($"O valor atribuido a variavel a é: {a}");
+            Console.WriteLine($"Na verificação se [a != 10] o resultado é: {comparacao5}");
+            Console.WriteLine($"Na verificação se [a == 10] o resultado é: {comparacao6}");
+            Console.WriteLine($"Na verificação se [a >= 10] o resultado é: {comparacao7}");
+            Console.WriteLine($"Na verificação se [a <= 10] o resultado é: {comparacao8}");
+
+            #endregion
+
+            #region Area de Encerramento do Programa
+            Console.WriteLine("\n\nPressione qualquer tecla para sair....");
+            Console.ReadKey();
+            Console.Clear();
+            #endregion
+        }
+    }
+}
---
+++ Aula12_OperadoresLogicos/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Aula12_OperadoresLogicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Area de Declaração de Vars
+            Console.Clear();
+
+            bool comparacao1, comparacao2, comparacao3, comparacao4, comparacao5, comparacao6, comparacao7;
+
+            #endregion
+
+            #region Processamento de Dados
+
+            comparacao1 = 4 != 5;
+            comparacao2 = 2 > 3;
+            comparacao3 = 2 > 3 && 4 != 5;  //&& Representa [ e ] 
+            comparacao4 = 2 > 3 || 4 != 5;  // || Representa [ ou ]
+            comparacao5 = !(2 > 3) && 4 != 5;  // ! antes de algum numero ou texto inverte o resultado 
+
+            /*
+            && (e) - PRECISA QUE TODOS DEEM TRUE PARA RETORNAR TRUE
+            || (OU) - SE UMA ÚNICA CONDIÇÃO DER TRUE ELE RETORNA TRUE
+            ! (NEGAÇÃO) - Inverte o resultado. ELe nega o valor obtido
+            */
+
+            //Comparando resultado e nao numero
+            comparacao6 = comparacao1 || comparacao3 && comparacao4;
+            comparacao7 = (comparacao1 || comparacao3) && comparacao2;
+
+            #endregion
+
+            #region Bloco de saida de dados
+
+            Console.WriteLine("----------------------------------------------------------------------------------");
+            Console.WriteLine("*** Saída de dados das comparações E(&&) OU(||) ***");
+            Console.WriteLine("----------------------------------------------------------------------------------");
+            Console.WriteLine($" A comparação [4 != 5] tem resultado............: {comparacao1}");
+            Console.WriteLine($" A comparação [2 > 3] tem resultado.............: {comparacao2}");
+            Console.WriteLine($" A comparação [2 > 3 && 4 != 5] tem resultado...: {comparacao3}");
+            Console.WriteLine($" A comparação [2 > 3 || 4 != 5] tem resultado...: {comparacao4}");
+            Console.WriteLine($" A comparação [!(2 > 3) && 4 != 5] tem resultado: {comparacao5}");
+            Console.WriteLine($" A comparacao1 || comparacao3 && comparacao4 é..: {comparacao6}");
+            Console.WriteLine($" A (comparacao1 || comparacao3) && comparacao2 é: {comparacao7}");
+
+            #endregion
+
+            #region Area de Encerramento do Programa
+            Console.WriteLine("\n\nPressione qualquer tecla para sair....");
+            Console.ReadKey();
+            Console.Clear();
+            #endregion
+        }
+    }
+}
